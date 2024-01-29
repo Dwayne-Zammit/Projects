df2b6ff2f0611b7a61ac237742df6d9de24519d6
unknown <dwaynezammit1999@gmail.com>
2024-01-29 01:45:40 +0100

    organised some folders for vm staff

---
+++ rs_bot_2/VM Stuff/installRunelite.ps1
@@ -0,0 +1,12 @@
+# Define variables
+$RuneLiteURL = "https://github.com/runelite/launcher/releases/download/2.6.10/RuneLiteSetup.exe"
+$InstallerPath = "$env:TEMP\RuneLiteSetup.exe"
+
+# Download RuneLite setup executable
+Invoke-WebRequest -Uri $RuneLiteURL -OutFile $InstallerPath
+
+# Install RuneLite silently
+Start-Process -FilePath $InstallerPath -ArgumentList "/S" -Wait
+
+# Remove the installer file
+Remove-Item $InstallerPath
